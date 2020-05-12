e37c8e2f51cc7f6a1348ce3395aa2ec2d78008ec
ddmsist <64846249+ddmsist@users.noreply.github.com>
2020-05-12 14:22:21 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_xKMMqBupX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ctfrt14blejjalil8kc3dut5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://7w4szesby9.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
